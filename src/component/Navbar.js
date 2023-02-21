fdf6d767469ae7792e57cd8cdff9f438936c3ebb
AF Tamim <itamim345@gmail.com>
2023-02-21 23:36:55 +0530

    Started working with navbar

---
+++ src/component/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../Common.css"
+export default function Navbar() {
+  return (
+    <div>
+      <div className="top-bar">
+        <span>
+            +91 348347384
+        </span>
+        <span>
+            quality@leather.com
+        </span>
+      </div>
+      <nav></nav>
+    </div>
+  );
+}
