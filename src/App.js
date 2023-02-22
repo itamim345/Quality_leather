607024a13aef7cf6c12c1da49ac70fbdf09a9aed
AF Tamim <itamim345@gmail.com>
2023-02-22 22:42:59 +0530

    Added Banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Banner from './component/Banner';
 import OurNav from './component/OurNav';
 
 function App() {
   return (
     <div className="App">
       <OurNav/>
+      <Banner/>
     </div>
   );
 }
---
+++ src/component/Banner.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+import "../Common.css"
+
+export default function Banner() {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+      setIndex(selectedIndex);
+    };
+  return (
+    <div>
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item className="c-item">
+          <img
+            className="d-block w-100"
+            src="https://images.pexels.com/photos/3839432/pexels-photo-3839432.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item className="c-item">
+          <img
+            className="d-block w-100"
+            src="https://images.pexels.com/photos/3839432/pexels-photo-3839432.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item className="c-item">
+          <img
+            className="d-block w-100"
+            src="https://images.pexels.com/photos/3839432/pexels-photo-3839432.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
